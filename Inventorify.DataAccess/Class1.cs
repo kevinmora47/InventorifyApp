0250b3a7ec2abc8954cf93a0ab1962284279b4ee
Kevin Mora <kevinmora47@gmail.com>
2023-05-21 12:40:58 -0600

    Add project files.

---
+++ Inventorify.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventorify.DataAccess
+{
+    public class Class1
+    {
+    }
+}
